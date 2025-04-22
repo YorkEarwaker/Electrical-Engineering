5837ab7a6dbdd559e848f35bdc338ded8b245736
York Earwaker <yorkearwaker@yahoo.com>
2025-04-22 11:16:30 +0100

    Delete rpi-pico directory
    tobe replaced

--- rpi-pico/mpy/hwd/src/org/agw/een/hwd/hello_world.py
+++
@@ -1,8 +0,0 @@
-
-# https://thepihut.com/blogs/raspberry-pi-tutorials/raspberry-pi-pico-getting-started-guide
-# Hello World
-
-print("This is my Pico talking!")
-print("Hello World!")
-print("Duck Soup")
-print("A Night At The Opera")
--- rpi-pico/mpy/hwd/src/org/agw/een/hwd/light_pico_onboard_led.py
+++
@@ -1,9 +0,0 @@
-
-# https://thepihut.com/blogs/raspberry-pi-tutorials/raspberry-pi-pico-getting-started-guide
-# light RPi Pico 2 W onboard led
-# GPIO pin 25, GPIO25, 'LED'
-# 1 = HIGH (on), 0 = LOW (off)
-
-from machine import Pin
-onboardLED = Pin('LED', Pin.OUT)
-onboardLED.value(1)
