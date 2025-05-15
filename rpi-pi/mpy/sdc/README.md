e7dc1285f2ede67cdd693226b904489bc68565c7
York Earwaker <yorkearwaker@yahoo.com>
2025-05-15 17:10:21 +0100

    update

--- rpi-pi/mpy/sdc/README.md
+++ rpi-pi/mpy/sdc/README.md
@@ -2,19 +2,22 @@
 
 RPi Pico SD Card SPI interface, memory extension for RPi Pico to extend life of Pico flash memory, eMMC and SD
 
+Primary use case in relation to \amn project for data logging time series data. 
+
 ## Status
 
 TODO
-* <todo: SD Card extension for RPi Pico, breakout board/shield/hat ,  >
+* <todo: SD Card extension for RPi Pico, breakout board/shield/hat, candidate for first project roll your own PCB? perf board? >
 * <todo: consider, SDIO support rpi pico, not yet part of official SDK as of 06/05/2025? investigate prototype library that includes 1 and 4 bit SDIO support using PIO, up to 4 data pins at once,  SD Card use case, other use cases?, >
 * <todo: consider, I2C support RPi Pico, libraries to source, 3rd party projects to identify, >
 * <todo: consider, determine if Pololu device can be wired to Pico for dual use, SPI and SDIO, using same pinout but different mpy code pin allocation? Pololu device has eleven (11) pins >
 * <todo: consider, determine if Pololu device can be wired to Pico for dual use, SPI and SDIO, using same pinout but different mpy code pin allocation?
+* <todo: consider, level shifters and voltage dividers, for devices/system integration with logic level signals above V3.3, further research required, components tobe identified, this should probably be part of a seperate PoC of mixed voltage systems integration, >
 
 DONE
 * <done: consider, SD Card storage project to increase total storage above onboard 2MB RPi Pico flash storage, MicroPython taking up to 600kB storage and available storage reduced to 1448kB, limiting ~20k flash writes/delete cycles lifetime, falsh is not replacable, SD Card extension is replaceable component, Weather station project \amn might benefit from this solution central data storage hub 'mother' Pico and spoke 'children' Pico's sensor control data logging to data hub 'mother' SD Card, good use of componentisation, loose coupling, high cohesion, . spoke 'child' low spec Pico or even lower spec MCU?  >
 * <done: consider, MicroPython library support for, SD Cards, eMMC cards, SD vs eMMC,  sdcard.py mpy driver for SD Cards, FAT32 formatted best option for pico? >
-
+* <done: consider, extension cable, for micro SD Card, improve life of sd card, ability to move sd card between sd card breakout boaords, ability to use sd card in RPi 5 or laptop, >
 
 ## Libraries
 
@@ -39,20 +42,24 @@ Libs
 
 ## Hardware
 
-SD Card Boards - assessment, evaluation, RPi Pico 2 W, system/device voltage v3.3 
+SD Card 'reader' boards - assessment, evaluation, RPi Pico 2 W, system/device voltage v3.3 
 * Adafruit Micro SD SPI or SDIO Card Breakout Board, The Pi Hut suppler [WS](https://thepihut.com/products/adafruit-micro-sd-spi-or-sdio-card-breakout-board-3v-only)
 * Pololu Breakout Board for MicroSD Cards, The Pi Hut suppler [WS](https://thepihut.com/products/pololu-breakout-board-for-microsd-cards), product [WS](https://www.pololu.com/product/2597), schematic [PDF](https://www.pololu.com/file/0J808/breakout-board-for-microsd-card-schematic.pdf), Not v3.3 shifted for use in v5 systems integration, 
 * <todo: others to assess>
 
-SD Card Boards - assessment, evaluation, , system/device voltage v5 +
+Level shifters, voltage dividers  - assessment, evaluation, , system/device voltage greater than V3.3, RPi Pico (also RPi 5?) operates at V3.3, SD Cards operate between V2.7 V3.6 ? 
 * Pololu Logic Level Shifter, 4-Channel, Bidirectional, product [WS](https://www.pololu.com/product/2595), schematic [PDF](https://www.pololu.com/file/0J752/ls01a-logic-level-shifter-schematic.pdf), This logic level converter requires two supply voltages: the lower-voltage logic supply (1.5 V to 7 V) connects to the LV pin and the higher-voltage supply (LV to 18 V) connects to the HV pin. The HV supply must be higher than the LV supply for proper operation. Is this required for use with RPi Pico? probably not but an interesting solution for other dibirectional voltage high/low low/high. If RPi Pico has to integrate with higher voltage device/systems
+
+SD Card 'reader' boards - assessment, evaluation, , for use with system/device voltage v5 +
 * Pololu Breakout Board for microSD Card with 3.3V Regulator and Level Shifters, product [WS](https://www.pololu.com/product/2587), schematic [PDF](https://www.pololu.com/file/0J873/breakout-board-for-micro-sd-card-with-regulator-and-level-shifters-schematic.pdf), direct integrated into 5 V systems
 
 ## References
 
 Terms
 * SD Card, [WP](https://en.wikipedia.org/wiki/SD_card)
-* eMMC , MultiMediaCard
+* eMMC , MultiMediaCard, precursor standard to SD Card, slower, less data, 
+* Serial Periferal Interface SPI [WP](https://en.wikipedia.org/wiki/Serial_Peripheral_Interface)
+* Secure Digital Input Output SDIO, [WP](https://en.wikipedia.org/wiki/SD_card#SDIO_cards)
 
 News Papers - External storage, i.e. SD Card memmory extension to RPi Pico 
 * SD Card Read Write,  [WS](https://www.digikey.com/en/maker/projects/raspberry-pi-pico-rp2040-sd-card-example-with-micropython-and-cc/e472c7f578734bfd96d437e68e670050), Maker . io,  
--- rpi-pi/mpy/sdc/src/org/agw/een/sdc/sdc_pololu_hwd.py
+++ rpi-pi/mpy/sdc/src/org/agw/een/sdc/sdc_pololu_hwd.py
@@ -24,12 +24,25 @@
 # https://datasheets.raspberrypi.com/picow/pico-2-w-pinout.pdf
 # https://picow.pinout.xyz/
 #
+# Serial Periferal Interface SPI
+# https://en.wikipedia.org/wiki/Serial_Peripheral_Interface
+#
+# Secure Digital Interface
+# https://en.wikipedia.org/wiki/SD_card
+#
+# Secure Digital Input Output
+# https://en.wikipedia.org/wiki/SD_card#SDIO_cards
+# https://www.tech-faq.com/sdio.html
 # 
-# SD Card
+# SD Card board
 # Pololu Breakout Board for MicroSD Cards, Not v3.3 logic shifted for use in v5 systems integration, 
 # product, https://www.pololu.com/product/2597),
 # schematic, https://www.pololu.com/file/0J808/breakout-board-for-microsd-card-schematic.pdf
 #
+# SD Card pinout, SD card, micro SD card
+# https://pinoutguide.com/Memory/sdcard_pinout.shtml
+# https://allpinouts.org/pinouts/connectors/memory/secure-digital-sd-card/
+# https://www.electroniccircuitsdesign.com/pinout/sd-microsd-card-pinout.html
 #
 # Context diagram
 # Assuming only the microcontroller interacts with the device.
@@ -71,7 +84,7 @@
 #           |        |  Micro SD Card
 #           |        |  Storage
 #           <        |  e.g. 32GB
-#            |       |  SD Card.s use V3.3
+#            |       |  SD Card's use V3.3
 #            |_______|
 #                     
 #      -------------------
@@ -86,76 +99,132 @@
 #                                                                                       | --- | ----------- |
 # 
 # | ----------------------------------------------------------------------------------------------- |
-# | Raspberry Pi Pico, Pololu (POL), pin map, ISP0 and SDIO - wip                                   |
+# | Raspberry Pi Pico, Pololu (POL), pin map, ISP and SDIO - wip                                    |
 # | ------- | ----------- | ---- | ------------------ | ------ | ---- | ------------------ | ------ |
 # | POL pin | Function    | SPI  | RPi Pico           | Wire   | SDIO | RPi Pico           | Wire   |
-# |         |             |      | Use,  SPI0, pin NN |        |      | Use,  SD,   pin NN |        |  
+# |         |             |      | Use,  SPI1, pin NN |        |      | Use,  SD,   pin NN |        |  
 # | ------- | ----------- | ---- | ------------------ | ------ | ---- | ------------------ | ------ |
-# | 1       | GND (VSS)   | GND  |                    |        | GND  | GND,        pin ?? |        | 
-# | 2       | VDD         | VDD  |                    |        | VDD  | 3V3,        pin ?? |        | 
-# | 3       | DAT2        |      |                    |        |      | GP??, ??,   pin ?? |        | 
-# | 4       | IRQ/DAT1    |      |                    |        |      | GP??, ?? ,  pin ?? |        | 
-# | 5       | GND (VSS)   | GND  | GND,        pin 38 | Black  | GND  | GND,        pin 38 | Black  | 
-# | 6       | VDD         | VDD  | 3V3,        pin 36 | Red    | VDD  | 3V3,        pin 36 | Red    |
-# | 7       | DI          |      | GP??, ???,  pin ?? | Yellow |      | GP??, ???,  pin ?? |        |
-# | 8       | DO          |      | GP??, ???,  pin ?? |        |      | GP??, ???,  pin ?? |        |
-# | 9       | SCLK        |      | GP??, ???,  pin ?? |        |      | GP??, ???,  pin ?? |        |
-# | 10      | CS (Chip S) |      | GP??, ???,  pin ?? |        |      | GP??, ???,  pin ?? |        |
-# | 11      | CD (Card D) |      |                    |        |      | GP??, ???,  pin ?? |        | 
+# | 1       | GND (VSS)   | —    | Null?              | NC?    | GND  | GND,        pin 38 |        | 
+# | 2       | VDD         | —    | Null?              | NC?    | VDD  | 3V3,        pin 36 |        | 
+# | 3       | DAT2        | —    | Null               | NC     | DAT2 | GP??, ??,   pin ?? |        | 
+# | 4       | IRQ/DAT1    | —    | Null               | NC     | DAT1 | GP??, ?? ,  pin ?? |        | 
+# | 5       | GND (VSS)   | —    | GND,        pin 38 | Black  | GND  | GND,        pin 38 | Black  | 
+# | 6       | VDD         | —    | 3V3,        pin 36 | Red    | VDD  | 3V3,        pin 36 | Red    |
+# | 7       | DI          | MOSI | GP11, TX,   pin 15 | Yellow | CMD  | GP??, ???,  pin ?? |        |
+# | 8       | DO          | MISO | GP12, RX,   pin 16 |        | DAT0 | GP??, ???,  pin ?? |        |
+# | 9       | SCLK        | SCLK | GP10, SCK,  pin 14 |        | CLK  | GP??, ???,  pin ?? |        |
+# | 10      | CS (Chip S) | SS   | GP13, CSn,  pin 17 |        | DAT3 | GP??, ???,  pin ?? |        |
+# | 11      | CD (Card D) | —    | Null?              | NC?    | CD   | GP??, ???,  pin ?? |        | 
 # | ------- | ----------- | ---- | ------------------ | ------ | ---- | ------------------ | ------ |
-#
+# <todo: consider, if not using SD mode, assume POL pin's 1 and 2, GND (VSS) and VDD respectively,
+#        are not wired in SPI mode? >
+# <todo: consider, if using SD mode, assume POL pin's 1 and 2, GND (VSS) and VDD respectively,
+#        are wired in SD mode to RPi Pico GND pin 38 and 3V3 pin 36 respectively? >
 # 
 # | -------------------------------------------------------------------------------------------------------------- |
 # | SPI interface - wip                                                                                            |
 # | ------- | --------- | ------------------ | ------ | ---------------------------------------------------------- |
 # | POL pin | Function  | RPi Pico           | Wire   | Description                                                |
-# |         |           | Use,  SPI0, pin NN |        |                                                            |
+# |         |           | Use,  SPI1, pin NN |        |                                                            |
 # | ------- | --------- | ------------------ | ------ | ---------------------------------------------------------- |
 # | 5       | GND (VSS) | GND,        pin 38 | Black  | Ground                                                     |
 # | 6       | VDD       | 3V3,        pin 36 | Red    | 3.3V power input                                           |
-# | 7       | DI        | GP??,     , pin    |        |                                                            |
-# | 8       | DO        | GP??,     , pin    |        |                                                            |
-# | 9       | SCLK      | GP??,     , pin    |        |                                                            |
-# | 10      | CS        | GP??,     , pin    |        |                                                            |
-# | 11      | CD        | GP??,     , pin    |        | Null?                                                      |
+# | 7       | DI        | GP11, TX,   pin 15 |        | Data in, MOSI, Host transmist TX, serial data from RPi Pi  |
+# | 8       | DO        | GP12, RX,   pin 16 |        | Data out, MISO, Host recieve RX, serial data from device   |
+# | 9       | SCLK      | GP10, SCK,  pin 14 |        | Clock, clock signal from RPi Pi                            |
+# | 10      | CS        | GP13, CSn,  pin 17 |        | Chip select (active low)                                   |
+# | 11      | CD        | NULL?              | NC?    | Card detect <todo: remove this line?                       |
 # | ------- | --------- | ------------------ | ------ | ---------------------------------------------------------- | 
 #
 # | -------------------------------------------------------------------------------------------------------------- |
-# | SD  interface - wip                                                                                            |
+# | SD interface - wip                                                                                             |
 # | ------- | --------- | ------------------ | ------ | ---------------------------------------------------------- |
 # | POL pin | Function  | RPi Pico           | Wire   | Description                                                |
 # |         |           | Use,  SDIO, pin NN |        |                                                            |
 # | ------- | --------- | ------------------ | ------ | ---------------------------------------------------------- |
+# | 1       | VSS?      | GP??, ????, pin ?? |        |                                                            |
+# | 2       | VDD       | GP??, ????, pin ?? |        |                                                            |
+# | 3       | DAT2      | GP??, ????, pin ?? |        |                                                            |
+# | 4       | DAT1      | GP??, ????, pin ?? |        |                                                            |
+# | 5       | VSS?      | GP??, ????, pin ?? |        |                                                            |
+# | 6       | VDD       | GP??, ????, pin ?? |        |                                                            |
+# | 7       | CMD       | GP??, ????, pin ?? |        |                                                            |
+# | 8       | DAT0      | GP??, ????, pin ?? |        |                                                            |
+# | 9       | CLK       | GP??, ????, pin ?? |        |                                                            |
+# | 10      | DAT3      | GP??, ????, pin ?? |        |                                                            |
+# | 11      | CD        | GP??, ????, pin ?? |        | Card detect                                                |
+# | ------- | --------- | ------------------ | ------ | ---------------------------------------------------------- | 
 # 
-# -------------------- WIP
+# 
 # https://www.pololu.com/product/2597, retrieved 14/05/2025, 15:34, UK BST,
-# 
 # The following tables describe the function of each pin on the breakout board in SPI and SD mode:
+# | -------------------------------------------------------------------------------------------------------------- |
+# | Pololu (POL) pin out                                                                                           |
+# | ------- | --------- | ---------------------------------------------------------------------------------------- |
+# | POL Pin | Function  | Description                                                                              |
+# | ------- | --------- | ---------------------------------------------------------------------------------------- |
+# | 1, 5    | GND (VSS) | Power and logic ground                                                                   |
+# | 2, 6    | VDD       | Supply voltage (2.7 V to 3.6 V for standard microSD cards)                               |
+# | 11      | CD        | Card detect. When a card is inserted, this pin is floating; when no card is inserted,    |
+# |         |           | it is shorted to ground. A pull-up resistor can be used to pull the line high when       |
+# |         |           | a card is present.                                                                       |
+# | ------------------------------------------------------ | ----------------------------------------------------- |
+# |                        SPI Mode                        |                        SD Mode                        |
+# | ------- | --------- | -------------------------------- | ------- | --------- | ------------------------------- |
+# | POL Pin | Function  | Description                      | POL Pin | Function  | Description                     |
+# | ------- | --------- | -------------------------------- | ------- | --------- | ------------------------------- |
+# | 7       | DI        | Data in (MOSI)                   | 7       | CMD       | Command/response                |
+# | 8       | DO        | Data out (MISO)                  | 8       | DAT0      | Data (bit 0)                    |
+# | 9       | SCLK      | Clock                            | 9       | CLK       | Clock                           |
+# | 10      | CS        | Chip select (active low)         | 10      | DAT3      | Data (bit 3)                    |
+# | 4       | —         | Reserved                         | 4       | DAT1      | Data (bit 1)                    |
+# | 4       | IRQ       | Interrupt (active low;           | 4       | IRQ       | Interrupt (active low; SDIO     |
+# |         |           |            SDIO devices only)    |         |           |            SDIO devices only)   |
+# | 3       | —         | Reserved                         | 3       | DAT2      | Data (bit 2)                    |
+# | ------- | --------- | -------------------------------- | ------- | --------- | ------------------------------- |
+#  Warning: Standard microSD cards use 3.3 V logic level signals, so level shifters or voltage dividers
+#           are required when connecting one to a 5 V system.
 # 
-# Pin	Description
-# GND (VSS)	Power and logic ground
-# VDD	Supply voltage (2.7 V to 3.6 V for standard microSD cards)
-# CD	Card detect. When a card is inserted, this pin is floating; when no card is inserted, it is shorted to ground. A pull-up resistor can be used to pull the line high when a card is present.
-# SPI mode	SD mode
-# Pin	Description	Pin	Description
-# DI	Data in (MOSI)	CMD	Command/response
-# DO	Data out (MISO)	DAT0	Data (bit 0)
-# SCLK	Clock	CLK	Clock
-# CS	Chip select (active low)	DAT3	Data (bit 3)
-# —
-# IRQ	Reserved
-# Interrupt (active low; SDIO devices only)	DAT1
-# IRQ	Data (bit 1)
-# Interrupt (active low; SDIO devices only)
-# —	Reserved	DAT2	Data (bit 2)
-# Warning: Standard microSD cards use 3.3 V logic level signals, so level shifters or voltage dividers are required when connecting one to a 5 V system.
-# 
-# 
-# 
+# SD Card nine (9) pin pinout, micro sd card eight (8) pin pinout - wip
+# <todo: micro SD card 'pin out' map to SPI>
+# <todo: micro SD card 'pin out' map to SD interface >
+# <todo: micro SD card 'pin out' map to Pololu SD Card 'reader' board device >
+#
+# SD Card pinout
+#
+#     9  1  2  3  4  5  6 7 8
+#     ________________________
+#    /  [ ][ ][ ][ ][ ][ ][][]|        Pin  SD      SPI
+#   /[ ][ ][ ][ ][ ][ ][ ][][]|        1    CD/DAT3 CS
+#   |[ ][ ][ ][ ][ ][ ][ ][][]|        2    CMD     DI
+#   |[ ]                      |        3    VSS1    VSS1
+#   |                         |        4    VDD     VDD
+#   |                         |        5    CLK     SCLK
+#   |           SD           _|        6    VSS2    VSS2
+#   |                      _/          7    DAT0    DO
+#   |                    _/            8    DAT1    —
+#   |                  _/              9    DAT2    —
+#   |                _/
+#    ^-^-^-^-^-^-^-^
+#
+#  Micro SD Card pinout
+#
+#    1 2 3 4 5 6 7 8
+#    ________________
+#   |[][][][][][][][]|        Pin  SD      SPI
+#   |[][][][][][][][]|        1    DAT2    —
+#   |[][][][][][][][]|        2    CD/DAT3 CS
+#   |                |        3    CMD     DI
+#   |                |        4    VDD     VDD
+#  /      micro      |        5    CLK     SCLK
+#  --      SD        |        6    VSS     VSS
+#  /                 |        7    DAT0    DO
+#  |                 |        8    DAT1    —
+#  |_________________|
 #
 # Circuit diagram - work in progress
 # Simplified view of Rpi Pico microntroller and micro SD Card breakout board (device) circuit
-# Pin sequence numbers, left to right, 1 2 3 4 5 6 7 8 9 10 11, with circuit board and SD Card holder (SPI 'CRUD') forward facing
+# Pin sequence numbers, left to right, 1 2 3 4 5 6 7 8 9 10 11, with circuit board and SD Card 'reader' (SPI 'CRUD') forward facing
 # <todo: determine if Pololu device can be wired to Pico for dual use, SPI and SDIO, using same pinout but different mpy code pin allocation?>
 #            ________ 
 #           |        |  Micro SD Card
@@ -168,7 +237,7 @@
 #   1-|o|---__________--|o|-5  Simplified       | --- | ----------- | Device            | --- | ----------- | Device                                                                   
 #   2-|o|| |  ________  |o|-6  Front of         | 1   | GND (VSS)   | pins functions    | 5   | GND (VSS)   | pins functions    
 #   3-|o|| | |___||___| |o|-7  Micro SD Card    | 2   | VDD         |                   | 6   | VDD         |                   
-#   4-|o||_|            |o|-8  Holder           | 3   | DAT2        |                   | 7   | DI          |                   
+#   4-|o||_|            |o|-8  Reader           | 3   | DAT2        |                   | 7   | DI          |                   
 #     | |    | |    | | |o|-9  SPI 'CRUD'       | 4   | IRQ/DAT1    |                   | 8   | DO          |                   
 #     | |    | |    | | |o|-10 Pololu, ??       | --- | ----------- |                   | 9   | SCLK        |                   
 #     | |____|_|____|_|_|o|-11 Board                                                    | 10  | CS (Chip S) |
