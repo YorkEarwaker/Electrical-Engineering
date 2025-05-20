1985404eafdadf656a6ac96210cd958658465afd
York Earwaker <yorkearwaker@yahoo.com>
2025-05-20 16:59:18 +0100

    update

--- rpi-pi/mpy/sdc/src/org/agw/een/sdc/adc_potentiometer.py
+++ rpi-pi/mpy/sdc/src/org/agw/een/sdc/adc_potentiometer.py
@@ -50,38 +50,81 @@
 # system of systems IoT context B.
 # <todo: context text work in progress, last reviewed 01/05/2025, wip>
 #
+# Books
+# Getting started with, MicroPython on Raspberry Pi Pico, The Official Raspberry Pi Pico Guide
+# Chapter 8, Temperature Guage
+# Use your Raspberry Pi Pico's built in ADC to convert analogue
+# inputs, and also to read its internal temperature sensor
+#
 # Analogue to digital converter ADC
 # Is an internal part of the RPi Pico microcontroller RP2040 CPU and RP2035 CPU.
 # ADC resolution, tweleve (12) bits resolution, number of digital bits, 12 bit = 0 -4095, Micorpython converts to 16 bit = 65,535 
 # ADC channels, three (3) channels, number of analogue signals it can accept at any one time to convert to digital signal.
 # ADC channels, GPIO pins; GP26 ADC0, GP27 ADC1, GP28 ADC3, analogue channels 0, 1, 2, respectively, the three analogue channels, external
-# ADC channels, micorcontroller internal; ADC.CORE_TEMP, analogue channel 3, the fourth analogue channel, internal 
+# ADC channels, micorcontroller internal; ADC.CORE_TEMP, analogue channel 3, the fourth analogue channel, internal
 #
+# Potentiometer
+# Single-joint potentiometer, Adjustable potentiometer, 0~10K range, used for ADC test, volume/brightness adjustment, etc.
+# Waveshare, MicroPython learning kit
+# https://www.waveshare.com/raspberry-pi-pico-basic-kit.htm
+# https://www.waveshare.com/wiki/Raspberry_Pi_Pico # Potentiometer Experiment
+# https://www.passive-electroniccomponents.com/quality-11049880-16mm-carbon-potentiometer-wh148
+# https://www.handsontec.com/dataspecs/passive/WH148%20Pot-meter.pdf
+# https://quartzcomponents.com/products/wh148-single-potentiometer-b10k-3pin-15mm-2 # pinout, similar product
+# https://www.mouser.com/datasheet/2/414/P160-1545428.pdf
+#            _
+#  _____ __ | |-1   Simplified            | --- | ------------- | Device 
+# |_____|__|| |-2   Top down view         | 1   | VSS           | pins functions
+#           |_|-3   of potentiometor      | 2   | DAT, DO, MISO |
+#                                         | 3   | GND           |
+#                                         | --- | ------------- |
+#       _____                            _____    
+#      / B10k\      Simplified          /     \      Simplified      
+#     |  (|)  |     Front on view      |  W L [|     Back on view 
+#     |       |     of potentiometor   |\_____/|     of potentiometor
+#     |_o_o_o_|                        |_:_:_:_| 
+#       | | |                            | | |
+#       1 2 3                            3 2 1
+#   
 # Ciruit diagram
 # Analogue to digical converter ADC
 # Potentiometer
-#
+# <todo: consider removing 12C interface pin information from diagram, distracting? not necessary? >
 # 
-# <todo: finish wiring diagram, >
-# <todo: consider removing 12C interface pin information from diagram, distracting? not necessary? > 
-#                               | |                           
-#                               | |---------------------------------------|
-#                               |                                         |
-#                               |---------------------------------------| |
-#                                                                       | |
-#                                      RPi Pico 2 W pinout              | |
-#                                            _____                      | |
-#                                      -----|     |-----                | |
-#                                   1-| o ◯|_____|◯ o |-40            | |
-#                                   2-| o     USB     o |-39            | |
-#                                   3-| o             o |-38-----GND----| |
-#                                   4-| o             o |-37              |
-#                                   5-| o             o |-36---3V3(OUT)---|    
-#                                   6-| o  __         o |-35-------------ADC_VREF-
-#                                   7-| o |__| Flash  o |-34----GP28-------ADC2---
-#                                   8-| o   _______   o |-33----GND--------AGND---
-#                                   9-| o  | ARM   |  o |-32----GP27-------ADC1-----I2C1 SCL--
-#                                  10-| o  | 2035  |  o |-31----GP26-------ADC0-----I2C1 SDA--
+# Rotary potentiometer
+#            _
+#  _____ __ | |-1   Simplified            | --- | ------------- | Device 
+# |_____|__|| |-2   Top down view         | 1   | VSS           | pins functions
+#           |_|-3   of potentiometor      | 2   | DAT, DO, MISO |
+#                                         | 3   | GND           |
+#                                         | --- | ------------- |
+#       _____                            _____    
+#      / B10k\      Simplified          /     \      Simplified      
+#     |  (|)  |     Front on view      |  W L [|     Back on view 
+#     |       |     of potentiometor   |\_____/|     of potentiometor
+#     |_o_o_o_|                        |_:_:_:_| 
+#       | | |                            | | |
+#       1 2 3                            3 2 1                                                        
+#       | | |
+#       | |-)---------------------------------------------------------------|
+#       |   |                                                               | 
+#       |---)-------------------------------------------------------------| |
+#           |                                                             | |
+#           |-----------------------------------------------------------| | |
+#                                                                       | | |
+#                                      RPi Pico 2 W pinout              | | |
+#                                            _____                      | | |
+#                                      -----|     |-----                | | |
+#                                   1-| o ◯|_____|◯ o |-40            | | |
+#                                   2-| o     USB     o |-39            | | |
+#                                   3-| o             o |-38-----GND----| | |
+#                                   4-| o             o |-37              | |-------------------|
+#                                   5-| o             o |-36---3V3(OUT)---|                     |
+#                                   6-| o  __         o |-35-------------ADC_VREF-              |
+#                                   7-| o |__| Flash  o |-34----GP28-------ADC2---              |
+#                                   8-| o   _______   o |-33----GND--------AGND---              |
+#                                   9-| o  | ARM   |  o |-32----GP27-------ADC1-----I2C1 SCL--  |
+#                                  10-| o  | 2035  |  o |-31----GP26-------ADC0-----I2C1 SDA----|
 #                                  11-| o  |_______|  o |-30
 #                                  12-| o             o |-29
 #                                  13-| o             o |-28
