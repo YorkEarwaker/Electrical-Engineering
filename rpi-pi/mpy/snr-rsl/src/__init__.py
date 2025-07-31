774bbd7617a314759575594225e791470ef57432
York Earwaker <yorkearwaker@yahoo.com>
2025-07-31 16:27:38 +0100

    Delete rpi-pi/mpy/snr-rsl/src/__init__.py

--- rpi-pi/mpy/snr-rsl/src/__init__.py
+++
@@ -1,7 +0,0 @@
-# print(f'invoking __init__.py form {__name__}')
-# import org.agw.een.snr.snr_bme280_dvr # is this still incorrect? 
-#
-# __version__ = "0.1.0"
-# __author__ = "Citizen Developer"
-# 
-# print(f'sensor package v{__version__} initialised.')
